84e64fe0e1fef8f2e9fc8f4df53af8b8e7829537
bdstein33 <bdstein33@gmail.com>
2015-04-05 19:57:26 -0700

    Completed last and completed each

--- src/underbar.js
+++ src/underbar.js
@@ -50,7 +50,6 @@
     else{
       return array.slice(n-1, array.length);
     }
-
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -59,6 +58,18 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if( Array.isArray(collection)){
+        for (var i=0; i < collection.length; i+=1){
+        iterator(collection[i], i, collection);
+      }
+    }
+    else{
+      for (var key in collection){
+        iterator(collection[key], key, collection);
+      }
+    }
+    
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
